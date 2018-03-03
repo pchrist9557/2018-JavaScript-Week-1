5f169ab89f79aa7f2bfd6b5b5d5c5aba2f5f2662
Christine Phung <pchrist9557@gmail.com>
2018-03-03 12:37:58 -0600

    JavaScript

---
+++ test.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var name = "Christine";
+var born = 1991;
+var year = 2018;
+var age = year - born;
+
+console.log("My name is " + name);
+console.log("I am " + age + " years old");
+
+
+if( age > 21 ){
+	console.log("Damn... you are OLD!!");
+} else {
+	console.log("Dang... I was hoping to be younger than you.");
+}
+if ( age == 21 ) {
+	console.log("Soon you will be old as well.");
+} else {
+	console.log("I hate you!");
+}
